288cea23bf93d5875df42cbf8ed63ff0f3dd1f95
Yann <yann.dionisio@telecomnancy.eu>
2023-11-11 14:33:41 +0100

    [refactor] add src file in tests

--- tests/lexer/LexerTest.java
+++ tests/lexer/LexerTest.java
@@ -9,10 +9,15 @@ class LexerTest {
 
     @org.junit.jupiter.api.Test
     void testFileDoesNotExistException() {
-        assertThrows(FileNotFoundException.class, () -> new Lexer("tests/lexer/doesNotExist.adb"));
+        assertThrows(FileNotFoundException.class, () -> new Lexer("tests/src/doesNotExist.adb"));
     }
 
+    @org.junit.jupiter.api.Test
+    void testFileExists() {
+        assertDoesNotThrow(() -> new Lexer("tests/src/doesExist.adb"));
+    }
 
+/*
     @org.junit.jupiter.api.Test
     void scan() throws IOException {
         Lexer lexer = new Lexer("tests/unDebut.adb");
@@ -21,5 +26,5 @@ class LexerTest {
             token = lexer.scan();
             System.out.println(token);
         } while (token.getTag() != Tag.EOF);
-    }
+    }*/
 }
